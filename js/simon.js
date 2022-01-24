364c601b88d80249eba94f78af8a758f27e232aa
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-01-24 19:05:21 +0100

    Latest changes (to fix)

--- js/simon.js
+++ js/simon.js
@@ -3,16 +3,36 @@
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri  sono stati indovinati dall'utente.
 
+// Generare 5 numeri casuali e mostrarli in un alert all'utente.
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 let message = 'Memorizza questi numeri:';
+let randomNumbersArray = [];
 
 for (let i = 0; i < 5; i++){
     const randomNumber = getRandomNumber(1, 100);
+    randomNumbersArray.push(randomNumber);
     message += ' ' + randomNumber;
 }
+console.log(randomNumbersArray);
 alert(message);
 
+// Dall'ok  parte un timer di 30 secondi
+let askedNumbers;
+let isValid = false;
+let userNumbers = [];
 setTimeout(() => {
-    console.log('Test');
-}, 10000);
+    // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite prompt().
+    for (let i = 0; i < 5; i++){
+        const askNumber = parseInt(prompt('Inserisci uno dei numeri:'));
+        askedNumbers += ' ' + askNumber;
+        userNumbers.push(askNumber);
+    }
+    console.log(askedNumbers)
+    // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri  sono stati indovinati dall'utente.
+    if (userNumbers === randomNumbersArray){
+        console.log('OK');
+    } else {
+        console.log('No');
+    }
+}, 1000);
